1ea8e719c369dbb116c41fa1a09258155a922b16
diego fontecilla <mail@diegofontecilla.com>
2018-07-21 12:23:54 +0100

    Change generator working. Needs refurbish

--- lib/change_generator.rb
+++ lib/change_generator.rb
@@ -1,25 +1,39 @@
 class ChangeGenerator
-#  attr_reader :pound_pence
 
   def initialize(pounds_due)
     @pounds_due = pounds_due
-    @pound_denominations = [50, 20, 10, 5, 2, 1]
-    @pence_denominations = [0.50, 0.20, 0.10, 0.05, 0.02, 0.01]
-    @pounds_array = []
+    @denominations = [50, 20, 10, 5, 2, 1]
+    @change = []
   end
 
-  def separate_pounds_and_pence
+  def separate_pound_and_pence
     @pound_pence = @pounds_due.to_s.split(".")
   end
 
   def pound_change
     pounds = @pound_pence[0].to_i
-    @pound_denominations.each do |d|
-      if pounds >= d
-        @pounds_array << "£#{d}"
-        pounds -= d
-      end
+    while pounds > 0
+      denomination = @denominations.find { |d| pounds >= d }
+      @change << "£#{denomination}"
+      pounds -= denomination
     end
-    @pounds_array
+    @change
+  end
+
+  def pence_change
+    pence = @pound_pence[1].to_i
+    while pence > 0
+      denomination = @denominations.find { |d| pence >= d }
+      @change << "#{denomination}p"
+      pence -= denomination
+    end
+    @change
+  end
+
+  def give_me_the_change
+    separate_pound_and_pence
+    pound_change
+    pence_change
+    @change
   end
 end
--- spec/change_generator_spec.rb
+++ spec/change_generator_spec.rb
@@ -4,15 +4,60 @@ describe ChangeGenerator do
   context '#separate_pounds_and_pence' do
     it 'gets 12.00 and return an array with pounds and pence' do
       change_generator = ChangeGenerator.new(12.00)
-      expect(change_generator.separate_pounds_and_pence).to eq(["12", "0"])
+      expect(change_generator.separate_pound_and_pence).to eq(["12", "0"])
     end
   end
 
-  context '#pounds_into_notes' do
-    it 'gets 12.00 and return ["£10", "2"]' do
+  context '#pounds_into_notes_and_coins' do
+    it 'gets 12.00 and return ["£10", "£2"]' do
       change_generator = ChangeGenerator.new(12.00)
-      change_generator.separate_pounds_and_pence
-      expect(change_generator.pound_change).to eq ["£10", "£2"]
+      change_generator.separate_pound_and_pence
+      expect(change_generator.pound_change).to eq(["£10", "£2"])
+    end
+
+    it 'gets 13.00 and return ["£10", "£2", "£1"]' do
+      change_generator = ChangeGenerator.new(13.00)
+      change_generator.separate_pound_and_pence
+      expect(change_generator.pound_change).to eq(["£10", "£2", "£1"])
+    end
+
+    it 'gets 50.00 and return ["£50"]' do
+      change_generator = ChangeGenerator.new(50.00)
+      change_generator.separate_pound_and_pence
+      expect(change_generator.pound_change).to eq(["£50"])
+    end
+
+    it 'gets 100.00 and return ["£50", "£50"]' do
+      change_generator = ChangeGenerator.new(100.00)
+      change_generator.separate_pound_and_pence
+      expect(change_generator.pound_change).to eq(["£50", "£50"])
+    end
+
+    it 'gets 113.00 and return ["£50", "£50", "£10", "£2", "£1"]' do
+      change_generator = ChangeGenerator.new(113.00)
+      change_generator.separate_pound_and_pence
+      expect(change_generator.pound_change).to eq(["£50", "£50", "£10", "£2", "£1"])
+    end
+
+    it '#pence_change gets £100.12 and return ["10p", "2p"]' do
+      change_generator = ChangeGenerator.new(100.12)
+      change_generator.separate_pound_and_pence
+      expect(change_generator.pence_change).to eq(["10p", "2p"])
+    end
+
+    it 'gets 100.12 and return ["£50", "£50", "10p", "2p"]' do
+      change_generator = ChangeGenerator.new(100.12)
+      expect(change_generator.give_me_the_change).to eq(["£50", "£50", "10p", "2p"])
+    end
+
+    it 'gets 113.73 and return ["£50", "£50", "£10", "£2", "£1", "50p", "20p", "2p", "1p"]' do
+      change_generator = ChangeGenerator.new(113.73)
+      expect(change_generator.give_me_the_change).to eq(["£50", "£50", "£10", "£2", "£1", "50p", "20p", "2p", "1p"])
+    end
+
+    it 'gets 0.73 and return ["50p", "20p", "2p", "1p"]' do
+      change_generator = ChangeGenerator.new(0.73)
+      expect(change_generator.give_me_the_change).to eq(["50p", "20p", "2p", "1p"])
     end
   end
 end
